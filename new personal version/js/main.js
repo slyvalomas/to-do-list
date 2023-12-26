c87c0191b00ea867a5fe20c4ef024634253ecf07
Brahim Abdelmomen <118140084+slyvalomas@users.noreply.github.com>
2023-12-26 18:24:11 +0100

    v1

---
+++ new personal version/js/main.js
@@ -0,0 +1,116 @@
+
+const itemEntryForm=document.getElementById("itemEntryForm");
+const listItems=document.getElementById("listItems");
+const NewItem=document.getElementById("NewItem");
+const clearItems = document.getElementById("clearItems");
+
+
+
+let dataBase =[]
+
+//localStorage.setItem("realDataBase",JSON.stringify(dataBase));
+
+
+dataBase = JSON.parse(localStorage.getItem("realDataBase"))
+//-----
+//-----
+// itemEntryForm.addEventListener("submit",(event)=>{
+
+//     event.preventDefault();
+//     const itemValue=NewItem.value;
+//     dataBase.push(itemValue);
+//     console.log(dataBase);
+//     NewItem.value = "";
+//     let RealDataBase=localStorage.setItem("realDataBase",JSON.stringify(dataBase));
+//     // const getId=(x)=>{
+//     //     for (let i = 0;i<dataBase.length;i++ ){
+//     //         x = i ;
+//     //         console.log(x);
+//     //     }
+//     // }
+//     //getId()
+//     const  div=document.createElement("div")
+//     div.className="item"
+//     const checkbox = document.createElement("input")
+//     checkbox.type="checkbox"
+//     checkbox.className="check"
+//     checkbox.id= 1
+//     checkbox.tabIndex=0
+//     const label = document.createElement("label")
+//     label.htmlFor=1
+//     div.appendChild(checkbox)
+//     div.appendChild(label)
+//     listItems.appendChild(div)
+//     label.textContent=dataBase[1]
+// });
+clearItems.addEventListener("click",()=>{
+    checkY()
+});
+itemEntryForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    if (NewItem.value!=""){
+        const itemValue=NewItem.value;
+    dataBase.push(itemValue);
+    localStorage.setItem("realDataBase",JSON.stringify(dataBase));
+    NewItem.value = "";
+    }else{
+        console.log("add something")
+    }
+    
+});
+let NewData =JSON.parse(localStorage.getItem("realDataBase"))
+function renderPosts(){
+    for(let i=0;i<NewData.length;i++){
+    let RealDataBase=localStorage.setItem("realDataBase",JSON.stringify(dataBase));
+    const  div=document.createElement("div")
+    div.className="item"
+    const checkbox = document.createElement("input")
+    checkbox.type="checkbox"
+    checkbox.className="check"
+    checkbox.id= i
+    checkbox.tabIndex=0
+    const label = document.createElement("label")
+    label.htmlFor=i
+    label.textContent=NewData[i]
+    div.appendChild(checkbox)
+    div.appendChild(label)
+    listItems.appendChild(div)
+    }
+}
+renderPosts()
+
+
+function checkY(){
+    for(let i=0;i<NewData.length;i++){
+        const checkCheck=document.getElementById(`${i}`)
+        if (checkCheck.checked){
+           
+           dataBase.pop(i)
+           removeValue(i)
+        }else{
+            console.log("CHECK ANY BOX")
+        }
+        
+        }
+        
+}
+
+function removeValue(x){
+    Array.prototype.remove = function(...indicesToRemove) {
+        indicesToRemove.sort((a, b) => b - a);
+        indicesToRemove.forEach(index => {
+            if (index >= 0 && index < this.length) {
+                this.splice(index, 1);
+            } else {
+                console.log(`Index ${index} is out of bounds`);
+            }
+        });
+    };
+    dataBase = []
+    dataBase = JSON.parse(localStorage.getItem("realDataBase"))
+    console.log(dataBase)
+    dataBase.remove(x)
+    localStorage.setItem("realDataBase",JSON.stringify(dataBase));
+
+    console.log(dataBase)
+}
